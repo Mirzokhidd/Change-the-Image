f147f9b696381af46d1005e49a42b41d4a4e9404
Mirzokhidd <50044566+Mirzokhidd@users.noreply.github.com>
2020-03-08 18:53:31 +0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,147 @@
+function imageChange1() {
+    var image = document.getElementById("image1");
+    if (image.src.match("image1.jpeg"))
+    {
+        image.src = "image 2 (1).jpg";
+    }
+    else
+    {
+        image.src = "image1.jpeg";
+    }
+}
+
+function imageChange2() {
+    var image = document.getElementById("image2");
+    if (image.src.match("image11.jpeg"))
+    {
+        image.src = "image 2 (2).jpg";
+    }
+    else 
+    {
+        image.src = "image11.jpeg";
+    }
+}
+
+function imageChange3() {
+    var image = document.getElementById("image3");
+    if (image.src.match("image12.jpeg"))
+    {
+        image.src = "image_2(3).jpg";
+    }
+    else 
+    {
+        image.src = "image12.jpeg";
+    }
+}
+
+function imageChange4() {
+    var image = document.getElementById("image4");
+    if (image.src.match("image13.jpeg"))
+    {
+        image.src = "image_2(4).jpg";
+    }
+    else 
+    {
+        image.src = "image13.jpeg";
+    }
+}
+
+function imageChange5() {
+    var image = document.getElementById("image5");
+    if (image.src.match("image14.jpeg"))
+    {
+        image.src = "image2 (5).jpg";
+    }
+    else 
+    {
+        image.src = "image14.jpeg";
+    }
+}
+
+function imageChange6() {
+    var image = document.getElementById("image6");
+    if (image.src.match("image15.jpeg"))
+    {
+        image.src = "image2 (6).jpg";
+    }
+    else 
+    {
+        image.src = "image15.jpeg";
+    }
+}
+
+function imageChange7() {
+    var image = document.getElementById("image7");
+    if (image.src.match("image16.jpeg"))
+    {
+        image.src = "image2 (7).jpg";
+    }
+    else 
+    {
+        image.src = "image16.jpeg";
+    }
+}
+
+function imageChange8() {
+    var image = document.getElementById("image8");
+    if (image.src.match("image17.jpeg"))
+    {
+        image.src = "image2 (8).jpg";
+    }
+    else 
+    {
+        image.src = "image17.jpeg";
+    }
+}
+
+function imageChange9() {
+    var image = document.getElementById("image9");
+    if (image.src.match("image18.jpeg"))
+    {
+        image.src = "image2 (9).jpg";
+    }
+    else 
+    {
+        image.src = "image18.jpeg";
+    }
+}
+
+function imageChange10() {
+    var image = document.getElementById("image10");
+    if (image.src.match("image19.jpeg"))
+    {
+        image.src = "image2 (10).jpg";
+    }
+    else 
+    {
+        image.src = "image19.jpeg";
+    }
+}
+
+function imageChange11() {
+    var image = document.getElementById("image11");
+    if (image.src.match("image110.jpeg"))
+    {
+        image.src = "image2 (11).jpg";
+    }
+    else 
+    {
+        image.src = "image110.jpeg";
+    }
+}
+
+function imageChange12() {
+    var image = document.getElementById("image12");
+    if (image.src.match("image112.jpeg"))
+    {
+        image.src = "image2 (12).jpg";
+    }
+    else 
+    {
+        image.src = "image112.jpeg";
+    }
+}
+
+function buttonFunction() {
+    alert("Press the blocks to see what's behind. Press back to see the previous image");
+}
